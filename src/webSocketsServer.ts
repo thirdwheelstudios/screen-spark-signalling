54920e330184bca903fe302cab10bae4e205755c
Phil Crossland <phil@thirdwheelstudios.com>
2023-10-11 21:59:43 +0100

    Updated room naming convention

--- src/webSocketsServer.ts
+++ src/webSocketsServer.ts
@@ -4,7 +4,7 @@ import { apiKeyMiddleware } from './apiKeyMiddleware'
 
 const createWebSocketsServer = () => {
   const corsOrigins =
-    process.env.corsOrigins ??
+    process.env.CORS_ORIGINS ??
     'https://screenspark.co.uk,http://localhost:5173,http://127.0.0.1:5173'
 
   const httpServer = createServer()
@@ -19,17 +19,38 @@ const createWebSocketsServer = () => {
   io.on('connect', (socket) => {
     console.log(`connect ${socket.id}`)
 
-    socket.on('connectToShareId', (shareId: string) => {
-      socket.join(shareId)
-    })
+    const roomName = (shareId: string, isBroadcasting: boolean) => {
+      return `${shareId}-${isBroadcasting ? 'broadcaster' : 'viewer'}`
+    }
 
-    socket.on('iceCandidate', (shareId: string, candidate: RTCIceCandidate) => {
-      io.to(shareId).emit('iceCandidate', candidate)
-    })
+    socket.on(
+      'connectToShareId',
+      (shareId: string, isBroadcasting: boolean) => {
+        socket.join(roomName(shareId, isBroadcasting))
+      }
+    )
 
-    socket.on('sdp', (shareId: string, sdp: RTCSessionDescription | null) => {
-      io.to(shareId).emit('sdp', sdp)
-    })
+    socket.on(
+      'iceCandidate',
+      (
+        shareId: string,
+        isBroadcasting: boolean,
+        candidate: RTCIceCandidate
+      ) => {
+        io.to(roomName(shareId, isBroadcasting)).emit('iceCandidate', candidate)
+      }
+    )
+
+    socket.on(
+      'sdp',
+      (
+        shareId: string,
+        isBroadcasting: boolean,
+        sdp: RTCSessionDescription | null
+      ) => {
+        io.to(roomName(shareId, isBroadcasting)).emit('sdp', sdp)
+      }
+    )
 
     socket.on('disconnect', () => {
       console.log(`disconnect ${socket.id}`)
